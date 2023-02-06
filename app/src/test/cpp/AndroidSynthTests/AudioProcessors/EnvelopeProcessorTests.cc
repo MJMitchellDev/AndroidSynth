f6113b00f77844c47096d21f026e600c2e6cdacb
MJMitchellDev <michael.mitchell.dev@gmail.com>
2023-02-06 15:31:29 +0000

    small test refactor

--- app/src/test/cpp/AndroidSynthTests/AudioProcessors/EnvelopeProcessorTests.cc
+++ app/src/test/cpp/AndroidSynthTests/AudioProcessors/EnvelopeProcessorTests.cc
@@ -6,90 +6,92 @@
 
 using ::testing::Return;
 
-class MockAudioSource : public mjmitchelldev_androidsynth::AudioSource {
-    public:
-        MOCK_METHOD(float, GetSample, (), (override) );
-        MOCK_METHOD(void, OnPlaybackStopped, (), (override) );
-};
+namespace mjmitchelldev_androidsynth {
+    class MockAudioSource : public AudioSource {
+        public:
+            MOCK_METHOD(float, GetSample, (), (override) );
+            MOCK_METHOD(void, OnPlaybackStopped, (), (override) );
+    };
 
-TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenAttacking_AttacksLinearly) {
-    auto audioSource = std::make_shared<MockAudioSource>();
+    TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenAttacking_AttacksLinearly) {
+        auto audioSource = std::make_shared<MockAudioSource>();
 
-    EXPECT_CALL(*audioSource, GetSample())
-        .WillRepeatedly(Return(1.0f));
+        EXPECT_CALL(*audioSource, GetSample())
+            .WillRepeatedly(Return(1.0f));
 
-    auto envelopeGenerator = std::make_unique<mjmitchelldev_androidsynth::EnvelopeProcessor>(audioSource, 4.0f);
-    
-    //Set the attack to last 1 second, or 4 frames at a sample rate of 4
-    envelopeGenerator->SetAdsr(1.0f, 1.0f, 0.0f, 0.0f);
+        auto envelopeGenerator = std::make_unique<EnvelopeProcessor>(audioSource, 4.0f);
+        
+        //Set the attack to last 1 second, or 4 frames at a sample rate of 4
+        envelopeGenerator->SetAdsr(1.0f, 1.0f, 0.0f, 0.0f);
 
-    //expect a liner progression of volume;
+        //expect a liner progression of volume;
 
-    auto sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.0f);
+        auto sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.0f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.25f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.25f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.5f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.5f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.75f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.75f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 1.0f);
-}
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 1.0f);
+    }
 
-TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenDecaying_DecaysLinearly) {
-    auto audioSource = std::make_shared<MockAudioSource>();
+    TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenDecaying_DecaysLinearly) {
+        auto audioSource = std::make_shared<MockAudioSource>();
 
-    EXPECT_CALL(*audioSource, GetSample())
-        .WillRepeatedly(Return(1.0f));
+        EXPECT_CALL(*audioSource, GetSample())
+            .WillRepeatedly(Return(1.0f));
 
-    auto envelopeGenerator = std::make_unique<mjmitchelldev_androidsynth::EnvelopeProcessor>(audioSource, 4.0f);
-    
-    //Set the attack to last 1 second, or 4 frames at a sample rate of 4
-    envelopeGenerator->SetAdsr(0.0f, 1.0f, 1.0f, 0.0f);
-    envelopeGenerator->SetSustainGain(0.0f);
+        auto envelopeGenerator = std::make_unique<EnvelopeProcessor>(audioSource, 4.0f);
+        
+        //Set the attack to last 1 second, or 4 frames at a sample rate of 4
+        envelopeGenerator->SetAdsr(0.0f, 1.0f, 1.0f, 0.0f);
+        envelopeGenerator->SetSustainGain(0.0f);
 
-    //expect a liner decay of volume;
-    auto sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 1.0f);
+        //expect a liner decay of volume;
+        auto sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 1.0f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.75f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.75f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.5f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.5f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.25f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.25f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 0.0f);
-}
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 0.0f);
+    }
 
-TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenSustaining_KeepsValueGainConsistent) {
-    auto audioSource = std::make_shared<MockAudioSource>();
+    TEST(EnvelopeProcessorTests, EnvelopeProcessor_WhenSustaining_KeepsValueGainConsistent) {
+        auto audioSource = std::make_shared<MockAudioSource>();
 
-    EXPECT_CALL(*audioSource, GetSample())
-        .WillRepeatedly(Return(1.0f));
+        EXPECT_CALL(*audioSource, GetSample())
+            .WillRepeatedly(Return(1.0f));
 
-    auto envelopeGenerator = std::make_unique<mjmitchelldev_androidsynth::EnvelopeProcessor>(audioSource, 4.0f);
-    
-    //Set the attack to last 1 second, or 4 frames at a sample rate of 4
-    envelopeGenerator->SetAdsr(0.0f, 0.0f, 1.0f, 0.0f);
-    envelopeGenerator->SetSustainGain(1.0f);
+        auto envelopeGenerator = std::make_unique<EnvelopeProcessor>(audioSource, 4.0f);
+        
+        //Set the attack to last 1 second, or 4 frames at a sample rate of 4
+        envelopeGenerator->SetAdsr(0.0f, 0.0f, 1.0f, 0.0f);
+        envelopeGenerator->SetSustainGain(1.0f);
 
-    //expect a liner decay of volume;
-    auto sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 1.0f);
+        //expect a liner decay of volume;
+        auto sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 1.0f);
 
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 1.0f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 1.0f);
 
-    envelopeGenerator->SetSustainGain(1.5f);
-    sampleValue = envelopeGenerator->GetSample();
-    EXPECT_EQ(sampleValue, 1.5f);
+        envelopeGenerator->SetSustainGain(1.5f);
+        sampleValue = envelopeGenerator->GetSample();
+        EXPECT_EQ(sampleValue, 1.5f);
+    }
 }
--- app/src/test/cpp/AndroidSynthTests/AudioProcessors/SignalSummerTests.cc
+++ app/src/test/cpp/AndroidSynthTests/AudioProcessors/SignalSummerTests.cc
@@ -6,26 +6,28 @@
 
 using ::testing::Return;
 
-class MockAudioSource : public mjmitchelldev_androidsynth::AudioSource {
-    public:
-        MOCK_METHOD(float, GetSample, (), (override) );
-        MOCK_METHOD(void, OnPlaybackStopped, (), (override) );
-};
+namespace mjmitchelldev_androidsynth {
+    class MockAudioSource : public AudioSource {
+        public:
+            MOCK_METHOD(float, GetSample, (), (override) );
+            MOCK_METHOD(void, OnPlaybackStopped, (), (override) );
+    };
 
-TEST(EnvelopeProcessorTests, SignalSummer_WithTwoSources_AddsWithEqualWeight) {
-    auto audioSource = std::make_shared<MockAudioSource>();
-    auto audioSource2 = std::make_shared<MockAudioSource>();
+    TEST(EnvelopeProcessorTests, SignalSummer_WithTwoSources_AddsWithEqualWeight) {
+        auto audioSource = std::make_shared<MockAudioSource>();
+        auto audioSource2 = std::make_shared<MockAudioSource>();
 
-    EXPECT_CALL(*audioSource, GetSample())
-        .WillRepeatedly(Return(1.0f));
+        EXPECT_CALL(*audioSource, GetSample())
+            .WillRepeatedly(Return(1.0f));
 
-    EXPECT_CALL(*audioSource2, GetSample())
-        .WillRepeatedly(Return(1.5f));
+        EXPECT_CALL(*audioSource2, GetSample())
+            .WillRepeatedly(Return(1.5f));
 
-    std::vector<std::shared_ptr<mjmitchelldev_androidsynth::AudioSource>> sources = {audioSource, audioSource2};
+        std::vector<std::shared_ptr<AudioSource>> sources = {audioSource, audioSource2};
 
-    auto summer = std::make_unique<mjmitchelldev_androidsynth::SignalSummer>(sources);
-    auto sample = summer->GetSample();
+        auto summer = std::make_unique<SignalSummer>(sources);
+        auto sample = summer->GetSample();
 
-    EXPECT_EQ(sample, 2.5f);
+        EXPECT_EQ(sample, 2.5f);
+    }
 }
